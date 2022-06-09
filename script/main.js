fa6efa111cd1fc0a1dc6b30dc3ca1326469aedb2
sau <dontsendsamspam@gmail.com>
2022-06-09 13:29:42 -0700

    Rename variables to clearly represent rows and columns

--- script/main.js
+++ script/main.js
@@ -1,16 +1,16 @@
 let canvas = document.querySelector('.canvas');
-let canvasHeight = 16;
-let canvasWidth = 16;
+let rowsInCanvas = 16;
+let columnsInCanvas = 16;
 
 
-for(let i = 0 ; i <= canvasHeight ; i++) {
+for(let i = 0 ; i <= rowsInCanvas ; i++) {
   let row = document.createElement('div')
   row.style.display = 'flex';
   row.style.backgroundColor = 'rgb(224, 224, 224)';
   row.style.flex = '1';
   row.style.gap = '1px'
   
-  for(let i = 0 ; i <= canvasWidth ; i++) {
+  for(let i = 0 ; i <= columnsInCanvas ; i++) {
     let cell = document.createElement('div');
     cell.style.flex = '1';
     cell.style.backgroundColor = 'white';
